bfbb9964a314c7be1391ac2d43f9161e22afa3ee
Aleksandr Zakrevskij <ctapocta_210@mail.ru>
2021-03-26 15:21:10 +0300

    Complete with Magic Number

---
+++ Assets/Scripts/MagicNumber.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MagicNumber : MonoBehaviour
+{
+    public Text answer;
+    public Text count;
+    public Text endGame;
+    public int min = 1;
+
+    public int max = 1000;
+    private int i = 0;
+
+    private int guess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        answer.text = "Загадайте число от " + min +" до " + max;
+        count.text = "Затраченные ходы:";
+        endGame.text = "";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            UpdateGuess();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            UpdateGuess();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            EndGameMsg();
+        }
+
+        if (Input.GetKeyDown((KeyCode.Space)))
+        {
+            restartGame();
+            Start();
+        }
+    }
+
+    private void UpdateGuess()
+    {
+        guess = (min + max) / 2;
+        answer.text = "Это число вы загадывали? " + guess + "?";
+        i++;
+    }
+
+    private void restartGame()
+    {
+        min = 1;
+        max = 1000;
+        i = 0;
+    }
+
+    private void EndGameMsg()
+    {
+        count.text = "На угадывание затрачено ходов: " + i;
+        endGame.text = "Игра окончена!";
+    }
+}
---
+++ Assets/Scripts/SumNumber.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SumNumber : MonoBehaviour
+{
+    private int sum = 0;
+    private int toWin = 50;
+    private int sumTry = 0;
+    public Text bgnMsg;
+    public Text endMsg;
+
+    private KeyCode[] numPress =
+    {
+        KeyCode.Keypad0, KeyCode.Keypad1, KeyCode.Keypad2,
+        KeyCode.Keypad3, KeyCode.Keypad4, KeyCode.Keypad5,
+        KeyCode.Keypad6, KeyCode.Keypad7, KeyCode.Keypad8,
+        KeyCode.Keypad9
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgnMsg.text = "Нажимайте цифры от 1 до 9 на доп.клавиатуре";
+        updRule();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < numPress.Length; i++)
+        {
+            if (Input.GetKeyDown(numPress[i]))
+            {
+                sum += i;
+                updRule();
+            }
+        }
+
+        if (sum >= toWin)
+        {
+            endMsg.text =
+                $"Игра окончена. Сумма: {sum} \n, ходов затрачено: {sumTry} \nДля новой игры нажмите 'ПРОБЕЛ'";
+            //print("Игра окончена. Сумма: " + sum + "\n, ходов затрачено: "+sumTry);
+            //print("Для новой игры нажмите 'ПРОБЕЛ'");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sum = 0;
+            sumTry = 0;
+            updRule();
+        }
+    }
+
+    void updRule()
+    {
+        sumTry++;
+        endMsg.text = $"Нажимайте цифру\n Сумма: {sum}";
+        //print("Нажимайте цифру\n Сумма: "+sum);
+    }
+}
