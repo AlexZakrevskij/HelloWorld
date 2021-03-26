343a7a765fa4b4b7a46e079fc22c493f6f8b7d5a
Aleksandr Zakrevskij <ctapocta_210@mail.ru>
2021-03-26 12:32:19 +0300

    Init project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public int min = 1;
+
+    public int max = 1000;
+
+    private int guess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Make a wish your number from " + min +" to " + max);
+        UpdateGuess();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            UpdateGuess();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            UpdateGuess();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            print("Game Over!");
+        }
+    }
+
+    private void UpdateGuess()
+    {
+        guess = (min + max) / 2;
+        print("Are you wish that number " + guess +"?");
+    }
+}
