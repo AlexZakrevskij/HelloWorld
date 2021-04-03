83979f20494f6facf0fc7a6edb6be2513f1476d1
Aleksandr Zakrevskij <ctapocta_210@mail.ru>
2021-04-03 15:17:35 +0300

    Обновлено до 3го задания (бой игрока и противника)

---
+++ Assets/Scripts/RedEnemy.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedEnemy : MonoBehaviour
+{
+    //Переменные закрытые от Юнити
+    private string tpmEnemyName;
+    private int tpmEnemyHealth;
+    private int tpmEnemyDamage;
+    private int tpmEnemyPierce;
+    
+    //Переменные для правки внутри Юнити
+    public string enemyName;
+    public int enemyHealth;
+    public int enemyPierce;
+    public int enemyDamage;
+    
+    public Sprite enemySprite;
+    public SpriteRenderer spriteRenderer;
+    public MeshRenderer meshRenderer;
+    public TextMesh enemyTextMesh;
+
+    public RedPlayer goodGuy;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GetEnemyInfo();
+        SetEnemyInfo();
+        spriteRenderer.sprite = enemySprite;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            EnemyAttack();
+            UpdateAfterAttack(goodGuy.playerHealth);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetEnemyInfo();
+            goodGuy.SetPlayerInfo();
+        }
+    }
+    public void SetEnemyInfo()
+    {
+        enemyName = tpmEnemyName; 
+        enemyHealth = tpmEnemyHealth; 
+        enemyDamage = tpmEnemyDamage;
+        enemyPierce = tpmEnemyPierce;
+        enemyTextMesh.text = $"Имя: {tpmEnemyName}\nЗдоровье: {tpmEnemyHealth}";
+    }
+    public void EnemyAttack()
+    {
+        int damage = (tpmEnemyDamage + tpmEnemyPierce) - goodGuy.playerArmor;
+        if (damage > 0)
+        {
+            goodGuy.playerHealth -= damage; 
+            print($"{tpmEnemyName} атаковал {goodGuy.playerName} и нанес {damage} урона,  у {goodGuy.playerName} осталось {goodGuy.playerHealth} после этой атаки"); 
+        }
+        else
+        {
+            damage = 0;
+            goodGuy.playerHealth -= damage;
+            print($"{tpmEnemyName} не нанес {goodGuy.playerName} урона во время атаки"); 
+        }
+        
+    }
+
+    public void UpdateAfterAttack(int hp)
+    {
+        if (hp<=0)
+        {
+            goodGuy.playerTextMesh.text = $"Имя: {goodGuy.playerName}\nСтатус: Мертв";
+            GameOver();
+        }
+        else
+        {
+            goodGuy.playerTextMesh.text = $"Имя: {goodGuy.playerName}\nЗдоровье: {hp}";
+        }
+    }
+    public void GameOver()
+    {
+        print($"{tpmEnemyName} убит, {goodGuy.playerName} победил в бою!");
+        print("Нажмите ПРОБЕЛ для продолжения");
+    }
+
+    private void GetEnemyInfo()
+    {
+        tpmEnemyName  = enemyName;
+        tpmEnemyHealth = enemyHealth;
+        tpmEnemyDamage = enemyDamage;
+        tpmEnemyPierce = enemyPierce;
+    }
+}
---
+++ Assets/Scripts/RedPlayer.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RedPlayer : MonoBehaviour
+{
+    
+    //Переменные закрытые от Юнити
+    private string tpmPlayerName;
+    private int tpmPlayerHealth;
+
+    private int tpmPlayerDamage;
+    private int tpmPlayerArmor;
+    
+    //Переменные для правки внутри Юнити
+    public string playerName;
+    public int playerHealth;
+
+    public int playerDamage;
+    public int playerArmor;
+    public SpriteRenderer spriteRenderer;
+    public Sprite playerSprite;
+   // public MeshRenderer meshRenderer;
+    public TextMesh playerTextMesh;
+
+    public RedEnemy badGuy;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GetPlayerInfo();
+        SetPlayerInfo();
+        spriteRenderer.sprite = playerSprite;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            PlayerAttack();
+            UpdateAfterAttack(badGuy.enemyHealth);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetPlayerInfo();
+            badGuy.SetEnemyInfo();
+        }
+    }
+
+    public void SetPlayerInfo()
+    {
+        playerName = tpmPlayerName; playerHealth = tpmPlayerHealth; playerDamage = tpmPlayerDamage; playerArmor = tpmPlayerArmor;
+        playerTextMesh.text = $"Имя: {tpmPlayerName}\nЗдоровье: {tpmPlayerHealth}";
+    }
+
+    public void PlayerAttack()
+    {
+        int damage = tpmPlayerDamage;
+        if (damage > 0)
+        {
+            badGuy.enemyHealth -= damage; 
+            print($"{tpmPlayerName} атаковал {badGuy.enemyName} и нанес {damage} урона, у {badGuy.enemyName} осталось {badGuy.enemyHealth} здоровья после атаки"); 
+        }
+        else
+        {
+            damage = 0;
+            badGuy.enemyHealth -= damage; 
+            print($"{tpmPlayerName} атаковал {badGuy.enemyName} и не нанес урона, {badGuy.enemyName} не потерял здоровье после атаки"); 
+        }
+        
+    }
+
+    public void UpdateAfterAttack(int hp)
+    {
+        if (hp<=0)
+        {
+            badGuy.enemyTextMesh.text = $"Имя: {badGuy.enemyName}\nСтатус: Умер";
+            GameOver();
+        }
+        else
+        {
+            badGuy.enemyTextMesh.text = $"Имя: {badGuy.enemyName}\nЗдоровье: {hp}";
+        }
+    }
+
+    public void GameOver()
+    {
+        print($"{tpmPlayerName} убил {badGuy.enemyName} и победил в этом бою!");
+        print("Нажмите ПРОБЕЛ для продолжения");
+    }
+
+    private void GetPlayerInfo()
+    {
+    tpmPlayerName  = playerName;
+    tpmPlayerHealth = playerHealth;
+    tpmPlayerDamage = playerDamage;
+    tpmPlayerArmor = playerArmor;
+    }
+    
+}
